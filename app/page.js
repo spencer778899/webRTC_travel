ee869a3450f9a9d089fff2cd6ebd2bd01acbbc4b
spencer778899 <spencer778899@gmail.com>
2024-02-25 18:02:57 +0800

    feat: Add selfie camera recording

--- app/page.js
+++ app/page.js
@@ -1,5 +1,27 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
 export default function Home() {
-  return (
-    <main />
-  );
+  const videoRef = useRef(null);
+  useEffect(() => {
+    const mediaStreamConstraints = {
+      audio: true,
+      video: true,
+    };
+
+    const handleMediaStreamError = (error) => {
+      console.error("navigator.getUserMedia error:", error);
+    };
+
+    const gotLocalMediaStream = (mediaStream) => {
+      videoRef.current.srcObject = mediaStream;
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(mediaStreamConstraints)
+      .then(gotLocalMediaStream)
+      .catch(handleMediaStreamError);
+  }, []);
+  return <video autoPlay ref={videoRef} />;
 }
