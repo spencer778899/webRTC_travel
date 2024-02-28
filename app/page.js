7e2b31f471b2d3e51b827c38bce63eea405f07cf
spencer778899 <spencer778899@gmail.com>
2024-02-28 23:21:21 +0800

    feat: stop capture function

--- app/page.js
+++ app/page.js
@@ -1,27 +1,49 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const STREAM_CONSTRAINTS = {
+  audio: false,
+  video: true,
+};
 
 export default function Home() {
+  const [mediaStream, setMediaStream] = useState(null);
   const videoRef = useRef(null);
-  useEffect(() => {
-    const mediaStreamConstraints = {
-      audio: true,
-      video: true,
-    };
 
+  useEffect(() => {
+    if (!mediaStream) return;
+    videoRef.current.srcObject = mediaStream;
+  }, [mediaStream]);
+
+  const startCapture = () => {
+    if (mediaStream) return;
     const handleMediaStreamError = (error) => {
       console.error("navigator.getUserMedia error:", error);
     };
 
     const gotLocalMediaStream = (mediaStream) => {
-      videoRef.current.srcObject = mediaStream;
+      setMediaStream(mediaStream);
     };
 
     navigator.mediaDevices
-      .getUserMedia(mediaStreamConstraints)
+      .getUserMedia(STREAM_CONSTRAINTS)
       .then(gotLocalMediaStream)
       .catch(handleMediaStreamError);
-  }, []);
-  return <video autoPlay ref={videoRef} />;
+  };
+
+  const stopCapture = () => {
+    const videoTracks = mediaStream.getVideoTracks();
+    videoTracks.forEach((stream) => {
+      stream.stop();
+    });
+    setMediaStream(null);
+  };
+  return (
+    <>
+      <video autoPlay ref={videoRef} />
+      <button onClick={() => startCapture()}>開始錄製</button>
+      <button onClick={() => stopCapture()}>結束錄製</button>
+    </>
+  );
 }
