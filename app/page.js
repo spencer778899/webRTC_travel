7cb7e2d7e6d38e87a429b090d898683135f92374
spencer778899 <spencer778899@gmail.com>
2024-03-06 00:13:11 +0800

    feat: video snapshot

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 const STREAM_CONSTRAINTS = {
@@ -9,7 +10,9 @@ const STREAM_CONSTRAINTS = {
 
 export default function Home() {
   const [mediaStream, setMediaStream] = useState(null);
+  const [imgUrl, setImgUrl] = useState("");
   const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
   useEffect(() => {
     if (!mediaStream) return;
@@ -36,14 +39,44 @@ export default function Home() {
     const videoTracks = mediaStream.getVideoTracks();
     videoTracks.forEach((stream) => {
       stream.stop();
+      videoRef.current.srcObject = null;
     });
     setMediaStream(null);
   };
+
+  const makeScreenshot = () => {
+    if (!mediaStream) return;
+    canvasRef.current.width = videoRef.current.videoWidth;
+    canvasRef.current.height = videoRef.current.videoHeight;
+    console.log(canvasRef.current);
+    canvasRef.current
+      .getContext("2d")
+      .drawImage(
+        videoRef.current,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height,
+      );
+    setImgUrl(canvasRef.current.toDataURL("image/png"));
+  };
+
   return (
     <>
-      <video autoPlay ref={videoRef} />
+      <video autoPlay ref={videoRef} controls />
       <button onClick={() => startCapture()}>開始錄製</button>
       <button onClick={() => stopCapture()}>結束錄製</button>
+      <br />
+      <canvas ref={canvasRef} />
+      {imgUrl && (
+        <Image
+          src={imgUrl}
+          width={canvasRef.current.width}
+          height={canvasRef.current.height}
+          alt="Picture of the author"
+        />
+      )}
+      <button onClick={() => makeScreenshot()}>視訊截圖</button>
     </>
   );
 }
